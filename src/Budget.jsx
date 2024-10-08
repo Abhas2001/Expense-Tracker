0f35de27219ecd179c24f8e470788aaface0095d
Abhas Kumar <abhassinha98@gmail.com>
2024-10-09 00:55:13 +0530

    rfggsdg

--- src/Budget.jsx
+++ src/Budget.jsx
@@ -17,7 +17,7 @@ const Budget = () => {
 
     console.log(mis);
     let x=0;
-     x=(mis*100)/5000;
+     x=(mis*100)/input;
 
      const handlebudget=()=>{
            setflag(true);
@@ -29,36 +29,52 @@ const Budget = () => {
       
 
       const handleadd = () =>{
+        if(input.length>0){
          setrinks(true);
+        }
       }
 
       const handleedit = () =>{
-        setinput("");
+        setrinks(false);
       }
       console.log(input);
   return (
     <>
-      
+      <div className='w-full flex flex-col gap-10 justify-center items-center'>
       
 
-      <div className='w-full h-20 bg-slate-400'>
-      <button className='bg-white' onClick={()=>{navigate(-1)}}>back</button>
-      <button onClick={handlebudget}>create budget</button>
+      <div className='w-[50%] h-40 bg-[#2a2a2a]'>
+      <button className='text-white' onClick={()=>{navigate(-1)}}>back</button>
+      <button className='text-white' onClick={handlebudget}>create budget</button>
       <div>
-        <input type="text" onInput={handleinput} />
-        <button  onClick={handleadd}>add</button>
-        <button onClick={handleedit}>edit</button>
+        <input className='bg-slate-400' type="text" onInput={handleinput} />
+        <button className='text-white'  onClick={handleadd}>add</button>
+        <button  className='text-white' onClick={handleedit}>edit</button>
 
         {
           rinks == true?
-          <div>{input}</div>
+          <div className='text-white'>{input}</div>
           :
           <div></div>
         }
       </div>
-      <div className='w-[250px] h-[30px] border-2 border-white'><div style={{ width: `${(x*250)/100}px` }} className={`h-[28px]  bg-red-500`}></div></div>
+      <div className='w-[250px] h-[10px] border-2 border-white rounded-full'><div style={{ width: `${(x*250)/100}px` }} className={`h-[8px]  bg-green-500`}></div></div>
       </div>
+        
 
+          <div className='w-[50%] h-40 bg-[#2a2a2a] p-4'>
+            <div className='w-[60%] flex gap-5 justify-center items-center'>
+              <div className='w-[80px] h-[50px]'>
+              <img src="https://cdn.uconnectlabs.com/wp-content/uploads/sites/33/2023/01/goals.png" alt="" srcset="" />
+              </div>
+              <div>
+               <h1 className='text-white font-bold text-2xl'>Goals</h1>
+               <p className='text-gray-400 font-serif text-lg'>Set your own goal and a have a quick  <br />overview of your progress</p>
+               </div>
+               </div>
+               <button className='text-blue-500 font-bold text-xl'>Create Goal</button>
+          </div>
+          </div>
     </>
   )
 }
