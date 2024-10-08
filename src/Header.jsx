72502ff9e1d94ae62cedaea044e1283e7b99fd45
Abhas Kumar <abhassinha98@gmail.com>
2024-10-08 21:29:28 +0530

    denhfjsndfk

--- src/Header.jsx
+++ src/Header.jsx
@@ -42,7 +42,7 @@ const Header = (props) => {
     <div className='min-h-32'>
     <div className='w-full min-h-20 bg-[#1e1e1e] shadow-custom-white'>
       <h1 className="text-2xl font-bold  text-white">
-       Personal Expensecdfgtrd
+       Personal Expense
     </h1>
     <div className='flex justify-center items-center gap-20'>
       <button className={`text-white text-xl font-bold cursor-pointer ${valss==0?'border-b-2':null}`} onClick={handleHome}>Home</button>
